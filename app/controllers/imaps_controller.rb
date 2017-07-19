84213c213b8b58e65c0ff562788f7420291bdf27
clayton-perszyk <clayperszyk@gmail.com>
2017-07-18 19:29:58 -0700

    add initial imap connection code

--- app/controllers/imaps_controller.rb
+++ app/controllers/imaps_controller.rb
@@ -14,10 +14,19 @@ class ImapsController < ApplicationController
     end
   end
 
+  def get_email_subjects
+      @imap = Imap.find(session[:imap_id])
+      @imap.get_email_subjects(search_params)
+  end
+
 
   private
 
   def imap_params
      params.require(:imap).permit(:email, :password, :host, :port, :ssl)
+  end
+
+  def search_params
+    params.require(:email)
   end
 end
